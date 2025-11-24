59a144c57051b101d55b05862cde225ff1dc9d24
Konrad <k.adamczyk81@gmail.com>
2025-11-24 20:07:08 +0100

    Add hand cursor to scrollup component

--- resources/views/components/scrollup.blade.php
+++ resources/views/components/scrollup.blade.php
@@ -1,6 +1,6 @@
  <!-- Scroll to Top Arrow -->
             <button id="scrollToTopBtn" type="button"
-                class="hidden fixed bottom-6 right-6 z-50 bg-gray-900 text-white rounded-full p-3 shadow-lg transition-opacity duration-300 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                class="hidden fixed bottom-6 right-6 z-50 bg-gray-900 text-white cursor-pointer rounded-full p-3 shadow-lg transition-opacity duration-300 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
                 aria-label="Przewiń do góry">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M5 15l7-7 7 7" />
