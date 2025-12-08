9925850891452c793b30f03e6ea1b4aa5dd35120
Konrad <k.adamczyk81@gmail.com>
2025-12-08 17:13:44 +0100

    Favicons and web app manifest added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,9 +29,11 @@
     <meta name="twitter:image" content="@yield('twitter_image', asset(config('seo.defaults.image')))">
     
     {{-- Favicons --}}
-    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.ico" sizes="32x32">
     <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+    <link rel="icon" href="/favicon-96x96.png" type="image/png" sizes="96x96">
     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+    <link rel="manifest" href="/site.webmanifest">
     
     {{-- Fonts --}}
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
