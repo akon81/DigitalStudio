b8eb5a9124e8915ab6cb38dfc67781df5e7363a3
Konrad Adamczyk <k.adamczyk81@gmail.com>
2025-12-09 13:25:50 +0100

    Update services config and app layout - google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,6 +43,18 @@
     @livewireStyles
     {!! CookieConsent::styles() !!}
     
+    @if(config('services.google_analytics.id'))
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google_analytics.id') }}"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', '{{ config('services.google_analytics.id') }}');
+    </script>
+    @endif
+    
     @stack('head')
 </head>
 <body>
