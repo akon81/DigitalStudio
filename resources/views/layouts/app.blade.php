f651284a3ebf51799df8342773cbcc89c13e0ae8
Konrad <k.adamczyk81@gmail.com>
2026-01-22 20:51:09 +0100

    Add Google Tag Manager integration to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-NWWCMTLD');</script>
+    <!-- End Google Tag Manager -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
@@ -74,6 +81,10 @@
     @stack('head')
 </head>
 <body x-data="cursorEffect()" @mousemove.window="trackCursor($event)" @mouseleave.window="hideCursor()" class="relative overflow-x-hidden">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NWWCMTLD"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     {{-- Magnetic Cursor Trail Effect --}}
     <div class="pointer-events-none fixed inset-0 z-10 transition-opacity duration-700 ease-out"
          :style="`opacity: ${glowOpacity}`">
